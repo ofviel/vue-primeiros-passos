2f0731a4061443c10083ae5abf31449431518651
ofviel <125498410+ofviel@users.noreply.github.com>
2023-09-25 00:45:34 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Hello World!</h1>
+    <h2>Hola, bienvenido a tus estudios de Vue JS</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Century, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
